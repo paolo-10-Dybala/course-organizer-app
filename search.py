b0b5ba3a4b7b1fb4c52c22663b413789bd268d46
Fraz032 <frazahmad7715@gmail.com>
2025-07-14 21:44:45 +0500

    Update search.py

--- search.py
+++ search.py
@@ -1,8 +1,12 @@
 from db import connect
-def search_course(name_query):
+
+def search_course(query):
     conn = connect()
     c = conn.cursor()
-    c.execute("SELECT * FROM courses WHERE name LIKE ?", ('%' + name_query + '%',))
+    c.execute("""
+        SELECT * FROM courses 
+        WHERE name LIKE ? OR instructor LIKE ?
+    """, ('%' + query + '%', '%' + query + '%'))
     results = c.fetchall()
     conn.close()
-    return results
+    return results
