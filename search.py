a8fa3deddf0133f98d51457cc94a645b02084c2f
Fraz032 <frazahmad7715@gmail.com>
2025-07-14 21:22:43 +0500

    Delete search.py

--- search.py
+++
@@ -1,8 +0,0 @@
-from db import connect
-def search_course(name_query):
-    conn = connect()
-    c = conn.cursor()
-    c.execute("SELECT * FROM courses WHERE name LIKE ?", ('%' + name_query + '%',))
-    results = c.fetchall()
-    conn.close()
-    return results
